09079df8e4918d63a98596c0b9fe5aa66ee36db7
Usman <usman.mughal@outlook.fr>
2024-11-24 17:55:13 +0100

    Init homepage design

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,24 @@
+import Link from 'next/link';
 import Head from '../components/Head';
+import styles from '../styles/HomePage.module.css';
 
-export default function Home() {
+export default function HomePage() {
   return (
     <>
-      <Head title="Usman Mughal | Home" />
-      <h1>Home Page</h1>
+      <Head title="Usman Mughal | Accueil" />
+      <div className={styles.container}>
+        <h6 className={styles.welcome}>Salut les passionnés ! Je suis</h6>
+        <h1 className={styles.title}>Usman Mughal</h1>
+        <h3 className={styles.subtitle}>&lt;!-- Développeur Web --&gt;</h3>
+        <div className={styles.buttons}>
+          <Link href="/about">
+            <button className={styles.contained}>Que puis-je faire ?</button>
+          </Link>
+          <Link href="/contact">
+            <button className={styles.outlined}>Contactez-moi</button>
+          </Link>
+        </div>
+      </div>
     </>
   );
 }
