42bfd502d7eaa356d67dada31fdb7c23ed3f4ade
Usman <usman.mughal@outlook.fr>
2024-12-02 18:58:28 +0100

    refacto + clean up

--- src/components/ContactCode.jsx
+++ src/components/ContactCode.jsx
@@ -1,40 +1,47 @@
 import styles from '../styles/ContactCode.module.css';
 
+const contactItems = [
+  {
+    social: 'email',
+    link: 'usman.mughal@outlook.fr',
+    href: 'mailto:usman.mughal@outlook.fr',
+  },
+  {
+    social: 'github',
+    link: 'usmanmughal',
+    href: 'https://github.com/umgh25',
+  },
+  {
+    social: 'linkedin',
+    link: 'usmanmughal',
+    href: 'https://www.linkedin.com/in/usman-m-55b4161b6/',
+  },
+];
+
 const ContactCode = () => {
   return (
     <div className={styles.code}>
       <p className={styles.line}>
         <span className={styles.className}>.socials</span> &#123;
       </p>
-      <p className={styles.line}>
-        &nbsp;&nbsp;&nbsp;email:{' '}
-        <a
-          href="mailto:usman.mughal@outlook.fr"
-          target="_blank"
-          rel="noopener"
-        >
-          usman.mughal@outlook.fr
-        </a>
-        ;
-      </p>
-      <p className={styles.line}>
-        &nbsp;&nbsp;&nbsp;github:{' '}
-        <a href="https://github.com/umgh25" target="_blank" rel="noopener">
-          usmanmughal
-        </a>
-        ;
-      </p>
-      <p className={styles.line}>
-        &nbsp;&nbsp;&nbsp;linkedin:{' '}
-        <a
-          href="https://www.linkedin.com/in/usman-m-55b4161b6/"
-          target="_blank"
-          rel="noopener"
-        >
-          usmanmughal
-        </a>
-        ;
-      </p>
+      {contactItems.slice(0, 8).map((item, index) => (
+        <p className={styles.line} key={index}>
+          &nbsp;&nbsp;&nbsp;{item.social}:{' '}
+          <a href={item.href} target="_blank" rel="noopener">
+            {item.link}
+          </a>
+          ;
+        </p>
+      ))}
+      {contactItems.slice(8, contactItems.length).map((item, index) => (
+        <p className={styles.line} key={index}>
+          &nbsp;&nbsp;{item.social}:{' '}
+          <a href={item.href} target="_blank" rel="noopener">
+            {item.link}
+          </a>
+          ;
+        </p>
+      ))}
       <p className={styles.line}>&#125;</p>
     </div>
   );
--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -1,19 +1,31 @@
 import Head from 'next/head';
 
-const CustomHead = ({ title = 'Usman Mughal' }) => {
+const CustomHead = ({ title }) => {
   return (
     <Head>
       <title>{title}</title>
       <meta
         name="description"
-        content="Usman Mughal est un développeur web passionné, créant des sites et des applications que vous allez adorer utiliser"
+        content="Usman Mughal est un développeur full stack passionné, créant des sites web et des applications que vous adorerez utiliser."
       />
       <meta
         name="keywords"
-        content="usman, usman mughal, mughal, web developer, usman web developer, usman developer, mern stack, usman portfolio"
+        content="usman mughal, usman, mughal, portfolio développeur web, usman développeur web, usman développeur, mern stack, portfolio usman mughal, vscode-portfolio"
       />
+      <meta property="og:title" content="Portfolio Usman Mughal" />
+      <meta
+        property="og:description"
+        content="Un développeur full-stack créant des sites web que vous aimerez utiliser."
+      />
+      <meta property="og:image" content="img à mettre" />
+      <meta property="og:url" content="lien du site à mettre" />
+      <meta name="twitter:card" content="summary_large_image" />
     </Head>
   );
 };
 
 export default CustomHead;
+
+CustomHead.defaultProps = {
+  title: 'Usman Mughal',
+};
